5f25b347422a36a42c3f55031a75130910cf47c8
Swakshwar Ghosh <ghoshswakshwar@gmail.com>
2025-07-20 12:07:29 +0530

    modified setup,py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="datainsight-lite",
+    version="0.1.2rc1",
+    description="One-line EDA report generator for pandas DataFrames.",
+    author="Swakshwar Ghosh",
+    author_email="ghoshswakshwar@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={"datainsight": ["templates/*.html"]},
+    install_requires=[
+        "pandas",
+        "matplotlib",
+        "seaborn",
+        "jinja2"
+    ],
+    url="https://github.com/swaksh-war/datainsight-lite",
+    project_urls={
+        "Bug Tracker": "https://github.com/swaksh-war/datainsight-lite/issues",
+        "Documentation": "https://github.com/swaksh-war/datainsight-lite#readme",
+        "Source Code": "https://github.com/swaksh-war/datainsight-lite",
+    },
+    entry_points={
+        "console_scripts": [
+            "datainsight-lite=datainsight.cli:main"
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+    ],
+    python_requires=">=3.7"
+)
